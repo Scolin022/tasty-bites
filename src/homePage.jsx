c93f032d4fe5afdccce960cd1255cf1340d9237f
Sam Colin <samantha.colin022@gmail.com>
2023-12-08 17:42:13 -0600

    basic HomePage created

---
+++ src/homePage.jsx
@@ -0,0 +1,12 @@
+// define child components at top level
+
+
+
+// parent component
+export default function Homepage () {
+    return (
+        <div>
+            <h1>This is a test.</h1>
+        </div>
+    );
+}
