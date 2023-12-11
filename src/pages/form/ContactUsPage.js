c55fcc159922788cbc4213722abde41f95d4c738
Sam Colin <samantha.colin022@gmail.com>
2023-12-11 01:09:21 -0600

    contact form component added

--- src/pages/form/ContactUsPage.js
+++ src/pages/form/ContactUsPage.js
@@ -1,7 +1,10 @@
+import { ContactForm } from './../../components/form/Forms';
+
 export default function ContactUs() {
     return (
         <div>
             <h1>Contact Page</h1>
+            <ContactForm />
         </div>
     );
 }
