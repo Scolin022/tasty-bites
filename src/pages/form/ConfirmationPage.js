76e6c74c7e9a08de253ba69b6937f4f8d0d1b141
Sam Colin <samantha.colin022@gmail.com>
2023-12-11 01:06:15 -0600

    renaming

--- src/pages/form/ConfirmationPage.js
+++ src/pages/form/ConfirmationPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function SignUpSucessful(){
     return (
         <div>
-            <h1>Account Created Sucessfully</h1>
+            <h1>Submission Sucessful</h1>
             <h2>
                 <Link to="/">Back to Recipes</Link>
             </h2>
